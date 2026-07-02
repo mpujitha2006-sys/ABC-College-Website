ced8ad845500323e623318e45ae1984e751d5e5c
mpujitha2006-sys <mpujitha2006@gmail.com>
2026-07-02 18:01:04 +0530

    fix middleware and deployment

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mysql = require("mysql2");
 const session = require("express-session");
 const app = express();
 
+app.use(express.json());
+
 app.use(
   session({
     secret: "abccollege",
@@ -12,11 +14,20 @@ app.use(
   }),
 );
 
+app.use(express.static("public"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Poojitha@251",
-  database: "abc_college",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
