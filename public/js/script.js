9740001d50a820cb21f4b6c4f790d7bb62c3300e
mpujitha2006-sys <mpujitha2006@gmail.com>
2026-07-02 18:45:40 +0530

    fix frontend js

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,3 @@
- // Contact Form Success Alert
-
-const form = document.querySelector("#contactForm");
-
-if (form) {
-  form.addEventListener("submit", function () {
-    localStorage.setItem("messageSent", "true");
-  });
-}
-
-if (localStorage.getItem("messageSent") === "true") {
-  alert("Message Sent Successfully!");
-  localStorage.removeItem("messageSent");
-}
-
-// Smooth Scroll
-
 document
   .querySelectorAll('a[href^="#"]')
   .forEach((anchor) => {
@@ -34,36 +17,7 @@ document
     );
   });
 
-// Back to Top Button
-
-const topBtn =
-  document.getElementById("topBtn");
-
-if (topBtn) {
-  window.addEventListener(
-    "scroll",
-    function () {
-      if (window.scrollY > 300) {
-        topBtn.style.display = "block";
-      } else {
-        topBtn.style.display = "none";
-      }
-    },
-  );
-
-  topBtn.addEventListener(
-    "click",
-    function () {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    },
-  );
-}
-
 // Admin Search
-
 const searchInput =
   document.getElementById(
     "searchInput",
@@ -75,7 +29,6 @@ if (searchInput) {
     function () {
       const value =
         this.value.toLowerCase();
-
       const rows =
         document.querySelectorAll(
           "tbody tr",
